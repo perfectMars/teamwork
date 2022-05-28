7053bd4a103ad9b3d6b72f32da447bd267b33325
lk <lkstudy5691>
2022-05-29 02:09:42 +0800

    提交项目到仓库

---
+++ questionnaire/src/test/java/com/mars/wx/questionnaire/QuestionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mars.wx.questionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
